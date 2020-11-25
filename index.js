030e2478122163276be2599a88d706b9e0437665
okdistribute <633012+okdistribute@users.noreply.github.com>
2020-11-24 19:15:34 -1000

    fix: Works on Chrome

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-var out = prompt("What's your name?")
-let utterance = new SpeechSynthesisUtterance("Hello, " + out + "!");
-speechSynthesis.speak(utterance)
+var el = document.getElementById("answer-phone")
+
+el.onclick = function () {
+  let utterance = new SpeechSynthesisUtterance("What's your name?");
+  speechSynthesis.speak(utterance);
+  var out = prompt("What's your name?");
+  utterance = new SpeechSynthesisUtterance("Hello, " + out + "!");
+  speechSynthesis.speak(utterance);
+}
