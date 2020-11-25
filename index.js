4d534fb3e074d8713c1e78c91d16aad664ad04e7
okdistribute <633012+okdistribute@users.noreply.github.com>
2020-11-24 19:09:56 -1000

    speak!

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-var tts = require('basic-tts')
-
 var out = prompt("What's your name?")
-
 let utterance = new SpeechSynthesisUtterance("Hello, " + out + "!");
-speechSynthesis.speak(utterance).then(() => {
-  console.log("The speaker has spoken!");
-}).catch((err) => {
-  console.trace(err)
-  console.log("Sigh...the speaker did not speak :(");
-});
-
-
-console.log("READY!");
-console.log(window.speechSynthesis.getVoices());
+speechSynthesis.speak(utterance)
